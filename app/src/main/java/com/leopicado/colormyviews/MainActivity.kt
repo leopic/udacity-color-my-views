686e57a6d3a0497bc21f3d4f3e34d9c94e6ad97f
Leo Picado <leonardo.picado@zenjob.com>
2023-12-20 23:21:55 +0100

    More progress

--- app/src/main/java/com/leopicado/colormyviews/MainActivity.kt
+++ app/src/main/java/com/leopicado/colormyviews/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.leopicado.colormyviews
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.leopicado.colormyviews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(R.layout.activity_main)
+        setListeners()
+
+        setContentView(binding.root)
+    }
+
+    private fun setListeners() {
+        val clickableViews =
+            listOf(
+                binding.boxOneText,
+                binding.boxTwoText,
+                binding.boxThreeText,
+                binding.boxFourText,
+                binding.boxFiveText,
+                binding.constraintLayout
+            )
+
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(it) }
+        }
+    }
+
+    private fun makeColored(view: View) {
+        when (view.id) {
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
+            R.id.box_four_text -> view.setBackgroundColor(Color.GREEN)
+            R.id.box_five_text -> view.setBackgroundColor(Color.YELLOW)
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }
