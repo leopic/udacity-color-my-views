8cd3442e0d7889cc58a3a5893d1e8c13af080975
Leo Picado <leonardo.picado@zenjob.com>
2023-12-22 13:00:07 +0100

    Introduce buttons and basic color swapping

--- app/src/main/java/com/leopicado/colormyviews/MainActivity.kt
+++ app/src/main/java/com/leopicado/colormyviews/MainActivity.kt
@@ -26,7 +26,10 @@ class MainActivity : AppCompatActivity() {
                 binding.boxThreeText,
                 binding.boxFourText,
                 binding.boxFiveText,
-                binding.constraintLayout
+                binding.constraintLayout,
+                binding.buttonRed,
+                binding.buttonYellow,
+                binding.buttonGreen
             )
 
         for (item in clickableViews) {
@@ -35,12 +38,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun makeColored(view: View) {
+        val boxes = listOf<View>(binding.boxOneText, binding.boxTwoText, binding.boxThreeText, binding.boxFourText, binding.boxFiveText)
+
         when (view.id) {
             R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
             R.id.box_two_text -> view.setBackgroundColor(Color.MAGENTA)
             R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_four_text -> view.setBackgroundColor(Color.GREEN)
-            R.id.box_five_text -> view.setBackgroundColor(Color.YELLOW)
+            R.id.box_five_text -> view.setBackgroundColor(Color.BLACK)
+
+            R.id.button_red -> boxes.forEach { it.setBackgroundColor(Color.RED) }
+            R.id.button_yellow -> boxes.forEach { it.setBackgroundColor(Color.YELLOW) }
+            R.id.button_green -> boxes.forEach { it.setBackgroundColor(Color.GREEN) }
+
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
